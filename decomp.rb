9cb0717bb0167b55decdcd4ec7111b16755fab15
ryan <ball.ryan.rb@gmail.com>
2021-06-10 15:02:16 -0600

    adds decomp.rb

---
+++ decomp.rb
@@ -0,0 +1,37 @@
+# Write a function that takes in two arrays of numbers and returns all the numbers that appear in both arrays.
+
+# Function with loop to print each number in the first array
+
+# Function with loop to print each number in the second array
+
+# Function to loop through the first array and check to check what numbers appear in both arrays
+
+
+
+# Write a function that takes in an array of numbers and returns the 3rd largest number.
+
+# Clarify
+# Can we use methods??
+input: numbers = [2, 3, 5, 23, 67]
+output: 5
+
+# Visualize
+
+# Decomposition
+# Function to sort the array in descending order and print the array
+# Function to sort the array in descending order and print the element at index 2, this would be the 3rd largest number
+
+
+# Write a function that takes in an array of strings and returns all the pairs of strings that have a total length of 8 characters.
+
+# Clarify
+# Does total length mean the length of two strings combined equals 8?
+# Should the pairs be two separate strings in an array or a combined string?
+input: string = ["five", "four", "three", "eight", "few", "two", "wildcard"]
+output: pairs = [["five", "four"], ["three", "few"], ["three", "two"], ["eight", "few"], ["eight", "two"]]
+
+# Visualize
+
+# Decomposition
+# Function to loop through array and print length of each element
+# Function to add the length of
